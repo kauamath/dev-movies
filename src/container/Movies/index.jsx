4d76020fa48f5270fd1fe24fe7be844d76306c3b
kauamath <medeiroskaua372@gmail.com>
2023-04-24 15:30:50 -0400

    added new pages

---
+++ src/container/Movies/index.jsx
@@ -0,0 +1,10 @@
+function Movies() {
+  return (
+    <div>
+      <h1>Filmes</h1>
+      <p>Essa é a Filmes</p>
+    </div>
+  )
+}
+
+export default Movies
---
+++ src/container/Series/index.jsx
@@ -0,0 +1,10 @@
+function Series() {
+  return (
+    <div>
+      <h1>Séries</h1>
+      <p>Essa é a séries</p>
+    </div>
+  )
+}
+
+export default Series
