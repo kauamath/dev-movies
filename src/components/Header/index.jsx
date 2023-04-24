f5cba69405e819876283317983aa0d3390d34155
kauamath <medeiroskaua372@gmail.com>
2023-04-24 16:51:11 -0400

    adding component header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import Logo from '../../assets/logo.png'
+
+function Header() {
+  return (
+    <div>
+      <img src={Logo} alt="logo-dev-movies" style={{ width: 500 }} />
+    </div>
+  )
+}
+
+export default Header
