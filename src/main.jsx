3da93aa628ca38623e0e18bd99b3469a70d400af
kauamath <medeiroskaua372@gmail.com>
2023-04-25 13:44:06 -0400

    createGlobalStyle

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import Router from './routes/routes'
+import GlobalStyles from './styles/globalStyles'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Router />
     </BrowserRouter>
+    <GlobalStyles />
   </React.StrictMode>
 )
