6df8d70ad66c6fc64e30a1d62bd6d7fd0f45dccd
kauamath <medeiroskaua372@gmail.com>
2023-04-26 10:32:10 -0400

    adding components with styled-components

---
+++ src/container/Home/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Background = styled.div`
+  background-image: url(${(props) => props.img});
+  height: 100vh;
+  background-position: center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+`
