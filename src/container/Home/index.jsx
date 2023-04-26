f6a881937106d3907907448d0ae3e16630712650
kauamath <medeiroskaua372@gmail.com>
2023-04-26 10:31:11 -0400

    leaving the API information dynamic with useState

--- src/container/Home/index.jsx
+++ src/container/Home/index.jsx
@@ -1,19 +1,29 @@
+import { useState } from 'react'
+
 import api from '../../services/api'
+import { Background } from './styles'
 
 function Home() {
+  const [movies, setMovies] = useState()
+
   async function getMovies() {
     const data = await api.get('/movie/popular')
 
+    setMovies(data.data.results[1])
     console.log(data)
   }
 
   getMovies()
 
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Essa é a home</p>
-    </div>
+    <>
+      {movies && (
+        <Background img="https://image.tmdb.org/t/p/original//lWqjXgut48IK5f5IRbDBAoO2Epp.jpg">
+          <h1>{movies.title}</h1>
+          <p>{movies.overview}</p>
+        </Background>
+      )}
+    </>
   )
 }
 
