a752b1aa227ed6751965aef8055916874a6520a6
kauamath <medeiroskaua372@gmail.com>
2023-04-26 10:34:18 -0400

    adding component header

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom'
 
-import Header from '../components/Header'
-
+/* import Header from '../components/Header'
+ */
 function DefaultLayout() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <Outlet />
     </>
   )
