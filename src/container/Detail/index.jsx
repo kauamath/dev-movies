a3f88275048fb09af5edefe98eefaa24a8846347
kauamath <medeiroskaua372@gmail.com>
2023-07-14 20:22:10 -0400

    Create PromisseAll

--- src/container/Detail/index.jsx
+++ src/container/Detail/index.jsx
@@ -1,6 +1,40 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import {
+  getMovieById,
+  getMovieCredits,
+  getMovieSimilar,
+  getMovieVideos
+} from '../../services/getData'
 import { Container } from './styles'
 
 function Detail() {
+  const { id } = useParams()
+  const [movie, setMovie] = useState()
+  const [movieVideos, setMovieVideos] = useState()
+  const [movieCredits, setMovieCredits] = useState()
+  const [movieSimilar, setMovieSimilar] = useState()
+
+  useEffect(() => {
+    async function getAllData() {
+      Promise.all([
+        getMovieById(id),
+        getMovieVideos(id),
+        getMovieCredits(id),
+        getMovieSimilar(id)
+      ])
+        .then(([movie, videos, credits, similar]) => {
+          setMovie(movie)
+          setMovieVideos(videos)
+          setMovieCredits(credits)
+          setMovieSimilar(similar)
+        })
+        .catch((error) => console.error(error))
+    }
+    getAllData()
+  }, [])
+
   return (
     <Container>
       <div>Detalhe</div>
