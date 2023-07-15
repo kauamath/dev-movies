25d8f30423e7079b057b1f1ced9eeea96f85fb49
kauamath <medeiroskaua372@gmail.com>
2023-07-14 20:23:57 -0400

    created API call functions

--- src/services/getData.js
+++ src/services/getData.js
@@ -39,10 +39,30 @@ export async function getTopPeople() {
   return results
 }
 
-export async function getMovie(movieId) {
+export async function getMovieVideos(movieId) {
   const {
     data: { results }
   } = await api.get(`/movie/${movieId}/videos`)
 
   return results[0]
+}
+
+export async function getMovieCredits(movieId) {
+  const { data } = await api.get(`/movie/${movieId}/credits`)
+
+  return data
+}
+
+export async function getMovieSimilar(movieId) {
+  const {
+    data: { results }
+  } = await api.get(`/movie/${movieId}/similar`)
+
+  return results
+}
+
+export async function getMovieById(movieId) {
+  const { data } = await api.get(`/movie/${movieId}`)
+
+  return data
 }
