8b9dbd3745124b36ae44c42749121edd44335b0e
kauamath <medeiroskaua372@gmail.com>
2023-07-24 10:18:12 -0400

    gender component created

---
+++ src/components/SpanGenres/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from './styles'
+
+function SpanGenres({ genres }) {
+  return (
+    <Container>
+      {genres &&
+        genres.map((genre) => <span key={genre.id}>{genre.name}</span>)}
+    </Container>
+  )
+}
+export default SpanGenres
---
+++ src/components/SpanGenres/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  margin-top: 30px;
+  gap: 10px;
+
+  span {
+    padding: 8px 18px;
+    border: 2px solid #ffffff;
+    border-radius: 30px;
+    font-size: 12px;
+    font-weight: 600;
+    background-color: #0f0f0f;
+    color: #ffffff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`
